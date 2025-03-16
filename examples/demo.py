bf8e46e3ef96d06fdb9f3d51299b946b9e9826af
shuixiaoyang <shuixiaoyang@pinduoduo.com>
2025-03-16 21:06:38 +0800

    feat: 完善demo.py

--- examples/demo.py
+++ examples/demo.py
@@ -15,18 +15,43 @@ if __name__ == "__main__":
     key_id = os.getenv("AQARA_KEY_ID")
     app_key = os.getenv("AQARA_APP_KEY")
 
-    client = AqaraClient(app_id, key_id, app_key, account="13482892612", token="d8f6b66ed3abcc5789c064841e654db2")
-
-    # client.send_auth_code()
-    # token_res = client.get_token("805859")
-    # print(token_res)
+    client = AqaraClient(app_id, key_id, app_key, account="Your Aqara Username")
+    # 会向你的手机发送一条验证码, 后续获取到Token的有效期可以自行指定，支持1~24h, 1~30d, 1~10y, 默认为30d
+    client.send_auth_code(token_validity='30d')
+    # 输入手机收到的验证码
+    code = input("Please input the code: ")
+    token_res = client.get_token(code)
+    token = token_res['accessToken']
+    refresh_token = token_res['refreshToken']
+    print(f"Token: {token}")
 
     controller = AqaraController(client)
     controller.load_data()
-    # print(json.dumps(controller.find_devices_by_name("开关", query_type=QueryType.FUZZY),
-    #                  cls=DefaultEncoder, indent=4, ensure_ascii=False))
-    control = controller.query_device().position_name("客厅").device_type(DeviceType.SWITCH).device_name("吧台开关").switch()[0]
-    print(control.is_on(2))
 
-    print(controller.scene().list_scene_names())
+    controller = AqaraController(client)
+    # 加载所有数据，如果数据在其他地方有更新（如在Aqara Home APP中更新了），需要重新调用一次此方法获取最新的数据
+    controller.load_data()
+
+    # 查询指定设备
+    devices = controller.query_device().position_name("客厅").device_type(DeviceType.LIGHT).device_name(
+        "我的射灯").query()
+
+    # 查询灯设备
+    light_controls = controller.query_device().position_name("客厅").device_type(DeviceType.LIGHT).device_name(
+        "我的射灯").light()
+
+    # 查询场景
+    all_scene_names = controller.scene().list_scene_names()
+    my_scene = controller.scene().get_scene_by_name("我的场景")
+
+    # 获取灯设备
+    my_light = controller.query_device().position_name("客厅").device_type(DeviceType.LIGHT).device_name("我的射灯").light()[0]
+    # 执行灯操作
+    if not my_light.is_on():
+        my_light.turn_on()
+    my_light.set_brightness(50)
+    my_light.set_color_temperature(4000)
+
+    # 执行场景
+    controller.scene().execute_scene("我的场景")
 
