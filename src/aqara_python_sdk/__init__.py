44598871f2f897f788aeeced145d11f51fbc7e1d
shuixiaoyang <shuixiaoyang@pinduoduo.com>
2025-03-17 14:00:08 +0800

    fix: 修复__init__.py非法import问题

--- src/aqara_python_sdk/__init__.py
+++ src/aqara_python_sdk/__init__.py
@@ -1,5 +1,5 @@
-import client
-import control
+import aqara_python_sdk.client as client
+import aqara_python_sdk.control as control
 
 AqaraClient = client.AqaraClient
 AqaraController = control.AqaraController
